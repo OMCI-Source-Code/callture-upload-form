78e05baf0e35bfe5e7d74f3f1810fa1a2000d5b2
MameMorM <mamemorm@my.yorku.ca>
2025-07-22 15:27:40 -0400

    Added google drive unit tests w/ some documentation

--- api/callture.py
+++ api/callture.py
@@ -49,7 +49,6 @@ def post_get_calls(cookies, line_no="All", ext_no="All", date_range=None):
         "DateRange": date_range,
         "Button": "Search",
     }
-
     req = httpx.post(CALL_LOG_URL, data=form_data, cookies=cookies, timeout=10.0)
     return req
 
@@ -61,20 +60,11 @@ def post_download_calls(cookies):
     return req
 
 
-def download_recording(recording: PersonRow):
-    print(f"Downloading {recording.CDRID}")
+async def download_recording(recording: PersonRow):
     line_number = recording.Line_No
     recording_id = recording.CDRID
     curr_file_url = DOWNLOAD_URL + str(line_number) + "&FileID=" + str(recording_id)
-    req = httpx.get(curr_file_url, timeout=10.0)
-    return req
-
-
-async def a_download_recording(recording: PersonRow):
-    print(f"Downloading {recording.CDRID}")
-    line_number = recording.Line_No
-    recording_id = recording.CDRID
-    curr_file_url = DOWNLOAD_URL + str(line_number) + "&FileID=" + str(recording_id)
+    print(f"Downloading {recording.CDRID} from {line_number}")
     try:
         async with httpx.AsyncClient() as client:
             req = await client.get(curr_file_url, timeout=100.0)
