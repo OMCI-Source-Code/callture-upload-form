4a3495019da625dbcd2b520eae2b84b5f380b90b
Anthony <tony.bart1511@gmail.com>
2025-07-24 21:17:54 +0000

    set upload endpoint to login required

--- api/__init__.py
+++ api/__init__.py
@@ -62,6 +62,7 @@ def create_app():
         flask_login.logout_user()
         return render_template("msg_logout.html")
 
+    @flask_login.login_required
     @app.route("/upload", methods=["POST"])
     def upload():
         def json_error_check(req):
